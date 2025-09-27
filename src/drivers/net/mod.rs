hermit-os/kernel

feat(drivers/net): Add loopback network driver
The idea is that in the immediate future, this driver will be the
fallback network driver. Once we support multiple interfaces, it should
always be the driver for the loopback interface.

Signed-off-by: Jens Reidel <adrian@travitia.xyz>

src/drivers/net/mod.rs
@@ -1,5 +1,13 @@
 #[cfg(all(target_arch = "riscv64", feature = "gem-net"))]
 pub mod gem;
+#[cfg(all(
+	not(any(
+		all(target_arch = "riscv64", feature = "gem-net", not(feature = "pci")),
+		all(target_arch = "x86_64", feature = "rtl8139"),
+	)),
+	feature = "loopback"
+))]
+pub mod loopback;
 #[cfg(all(target_arch = "x86_64", feature = "rtl8139"))]
 pub mod rtl8139;
 #[cfg(not(all(target_arch = "x86_64", feature = "rtl8139")))]
@@ -54,5 +62,31 @@ pub(crate) fn mtu() -> u16 {
 		u16::from_str(&my_mtu).unwrap()
 	} else {
 		DEFAULT_MTU
+	}
+}
+
+cfg_if::cfg_if! {
+	if #[cfg(all(
+		not(feature = "pci"),
+		not(all(
+			feature = "loopback",
+			not(all(target_arch = "riscv64", feature = "gem-net"))
+		))
+	))] {
+		pub(crate) use crate::arch::kernel::mmio::get_network_driver;
+	} else if #[cfg(all(
+		feature = "pci",
+		not(all(
+			feature = "loopback",
+			not(all(target_arch = "x86_64", feature = "rtl8139"))
+		))
+	))] {
+		pub(crate) use crate::drivers::pci::get_network_driver;
+	} else {
+		use hermit_sync::InterruptTicketMutex;
+
+		pub(crate) fn get_network_driver() -> Option<&'static InterruptTicketMutex<loopback::LoopbackDriver>> {
+			Some(&loopback::LOOPBACK)
+		}
 	}
 }

valopok/hermit-kernel

src/drivers/net/mod.rs
@@ -1,5 +1,13 @@
 #[cfg(all(target_arch = "riscv64", feature = "gem-net"))]
 pub mod gem;
+#[cfg(all(
+	not(any(
+		all(target_arch = "riscv64", feature = "gem-net", not(feature = "pci")),
+		all(target_arch = "x86_64", feature = "rtl8139"),
+	)),
+	feature = "net"
+))]
+pub mod loopback;
 #[cfg(all(target_arch = "x86_64", feature = "rtl8139"))]
 pub mod rtl8139;
 #[cfg(not(all(target_arch = "x86_64", feature = "rtl8139")))]
@@ -58,9 +66,27 @@ pub(crate) fn mtu() -> u16 {
 }
 
 cfg_if::cfg_if! {
-	if #[cfg(not(feature = "pci"))] {
+	if #[cfg(all(
+		not(feature = "pci"),
+		not(all(
+			feature = "net",
+			not(all(target_arch = "riscv64", feature = "gem-net"))
+		))
+	))] {
 		pub(crate) use crate::arch::kernel::mmio::get_network_driver;
-	} else {
+	} else if #[cfg(all(
+		feature = "pci",
+		not(all(
+			feature = "net",
+			not(all(target_arch = "x86_64", feature = "rtl8139"))
+		))
+	))] {
 		pub(crate) use crate::drivers::pci::get_network_driver;
+	} else {
+		use hermit_sync::InterruptTicketMutex;
+
+		pub(crate) fn get_network_driver() -> Option<&'static InterruptTicketMutex<loopback::LoopbackDriver>> {
+			Some(&loopback::LOOPBACK)
+		}
 	}
 }